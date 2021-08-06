ffe50b700bee02f019b62e29477bea18b6d09d27
bra1an152001 <87213830+bra1an152001@users.noreply.github.com>
2021-08-06 12:07:31 -0300

    Update sweetAlert.js

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -1,7 +1,7 @@
 Swal.fire({
   title: 'Medidas por Covid-19',
   
-  text: 'Recuerda que estamos atravezando una etapa muy dura, en la cual tenemos que cuidarnos lo maximo posible. Si tenies dos o más de los siguientes síntomas: fiebre de 37,5°C, tos, dolor de garganta, dificultad respiratoria, rinitis/congestión nasal, dolor muscular, cefalea, diarrea y/o vómitos. O si tenés solo pérdida brusca de gusto u olfato, aislate y consultá al sistema de salud de tu localidad.',
+  text: 'Recuerda que estamos atravezando una etapa muy dura, en la cual tenemos que cuidarnos lo maximo posible. Si tienes dos o más de los siguientes síntomas: fiebre de 37,5°C, tos, dolor de garganta, dificultad respiratoria, rinitis/congestión nasal, dolor muscular, cefalea, diarrea y/o vómitos. O si tenés solo pérdida brusca de gusto u olfato, aislate y consultá al sistema de salud de tu localidad.',
   
   footer: '<a href="https://www.argentina.gob.ar/salud/coronavirus-COVID-19">Mas informacion</a>'
 })
